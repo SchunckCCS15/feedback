815d3303e0fce1d90efd7a491ea44544a7d50a62
Cauã Schunck <branquelinho.schunck@gmail.com>
2024-06-10 00:59:05 +0000

    styled: estilização completa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,42 @@
-import { ButtonContainer, CardContainer, IconContainer } from "./Home.styles";
+import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from "./Home.styles";
 
 import IconStarImg from "../assets/icon-star.svg"
+import thankYou from "../assets/illustration-thank-you.svg"
 
 export  function Home() {
+ let aparecerResultado = true
+
   return (
-    <CardContainer>
-      <IconContainer>
-        <img src={IconStarImg} alt="Ícone de estrela" />
-      </IconContainer>
+    aparecerResultado === false ? (
+      <CardContainer>
+        <IconContainer>
+          <img src={IconStarImg} alt="Ícone de estrela" />
+        </IconContainer>
 
-      <h1>Como foi o atendimento?</h1>
-      <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
+        <h1>Como foi o atendimento?</h1>
+        <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
 
-      <ButtonContainer>
-         <button>1</button>
-         <button>2</button>
-         <button>3</button>
-         <button>4</button>
-         <button>5</button>
-      </ButtonContainer>
+        <ButtonContainer>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+        </ButtonContainer>
 
-      <button>Enviar</button>
-    </CardContainer>
+        <button>Enviar</button>
+      </CardContainer>
+    ) : (
+        <CardContainer>
+          <img src={ thankYou } alt="Imagen de agradecimento" />
+
+          <ResultContainer>
+            <p>Você selecionou 4 de 5</p>
+          </ResultContainer>
+
+          <h1>Obrigado!</h1>
+          <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suport, não hesite em entrar em contato!</p>
+        </CardContainer>
+    )
   )
 }
